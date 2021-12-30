d6b9083f25b1b5a2a83591a5654e0a35efd8c3e5
Anton <antojohansson1@gmail.com>
2021-12-30 14:22:41 +0100

    Temperature example now works

---
+++ src/temperature.h
@@ -0,0 +1,10 @@
+#ifndef TEMPERATUR_H
+#define TEMPERATURE_H
+
+
+//Take an analog signal from the given IO and returns temperature.
+//Need to be connected with a 10k resistor in order to work
+double getTemperature(int IO);
+
+
+#endif
